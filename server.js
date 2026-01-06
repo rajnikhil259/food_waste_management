a8d611e54cde421caf9f58a009de387467e338b3
rajnikhil259 <vishvajeet259@gmail.com>
2026-01-07 02:14:05 +0530

    connecting to neon database

--- server.js
+++ server.js
@@ -12,13 +12,10 @@ const PORT = process.env.PORT;
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 app.use(
   session({
